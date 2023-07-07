398d37fe9cb0ed18492d2ea426554db07f57de0a
anil-patil <panil8993@gmail.com>
2023-07-07 15:31:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <h2>React with Laravel</h2>
+        </div>
+    );
+}
+
+export default App;
